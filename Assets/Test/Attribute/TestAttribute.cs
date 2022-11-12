62123a91bbd860286262b374085d066743fff4d7
xzp3094997376 <3094997376@qq.com>
2022-11-12 10:42:07 +0800

    更新

---
+++ Assets/Test/Attribute/TestAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class TestAttribute : MonoBehaviour
+{
+    [EnumPaging, OnValueChanged("SetCurrentTool")]
+    [InfoBox("Example of using EnumPaging together with OnValueChanged.")]
+    public UnityEditor.Tool SceneTool;
+
+    private void SetCurrentTool()
+    {
+        UnityEditor.Tools.current = this.SceneTool;
+    }
+
+
+
+
+    [InfoBox("You can also assign the min max values dynamically by refering to members.")]
+    [MinMaxSlider("DynamicRange", true)]
+    public Vector2 DynamicMinMax;//最大最小值通过DynamicRange控制
+
+    [MinMaxSlider("Min", 10f, true)]
+    public Vector2 DynamicMin;//最小值通过DynamicRange.x控制
+
+    [MinMaxSlider(0f, "Max", true)]
+    public Vector2 DynamicMax;//最大值通过DynamicRange.y控制
+    
+    public Vector2 DynamicRange;
+
+    public float Min()
+    {   
+        return this.DynamicRange.x;
+    }
+    //public float Min { get { return this.DynamicRange.x; } }
+
+    public float Max { get { return this.DynamicRange.y; } }
+}
---
+++ Assets/Test/Serialize/Test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class Test : SerializedMonoBehaviour //要使用Odin的序列化功能需要改为从这个类型派生
+{
+    [BoxGroup("Data_A")]
+    public int[,] Data_A = new int[3, 3];
+    public int[][] Data_B = new int[3][];
+    public List<List<int>> Data_C = new List<List<int>>();
+    public Dictionary<string, int> Data_D = new Dictionary<string, int>();
+
+
+    public enum SomeEnum
+    {
+        First, Second, Third, Fourth, AndSoOn
+    }
+
+    public SomeEnum se;
+
+    [System.Flags]
+
+
+
+
+    public enum SomeBitmaskEnum
+    {
+        A = 1 << 1,
+        B = 1 << 2,
+        C = 1 << 3,
+        All = A | B | C
+    }
+
+    public SomeBitmaskEnum sonBitmaskEnum;
+}
